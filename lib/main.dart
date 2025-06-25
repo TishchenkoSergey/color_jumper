400b958851d3bb63edce5ed7c3814910eeeacff8
Serhii <tishchenko.seriy@gmail.com>
2025-06-25 11:48:55 +0300

    feat: init base navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,6 @@
-import 'package:flutter/material.dart';
+import 'package:color_jumper/app/app.dart';
+import 'package:color_jumper/bootstrap.dart';
 
 void main() {
-  runApp(const ColorJumperApp());
-}
-
-/// A simple Flutter app that demonstrates a color jumper.
-class ColorJumperApp extends StatelessWidget {
-  const ColorJumperApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp();
-  }
+  bootstrap(() => const App());
 }

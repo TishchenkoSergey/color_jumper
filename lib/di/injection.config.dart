940be2999155ab633467b012733f3d42f744dd5b
Serhii <tishchenko.seriy@gmail.com>
2025-06-25 21:21:26 +0300

    feat: update di config file

--- lib/di/injection.config.dart
+++ lib/di/injection.config.dart
@@ -9,6 +9,7 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:color_jumper/services/color_generator_service.dart' as _i847;
 import 'package:get_it/get_it.dart' as _i174;
 import 'package:injectable/injectable.dart' as _i526;
 
@@ -18,11 +19,13 @@ extension GetItInjectableX on _i174.GetIt {
     String? environment,
     _i526.EnvironmentFilter? environmentFilter,
   }) {
-    _i526.GetItHelper(
+    final gh = _i526.GetItHelper(
       this,
       environment,
       environmentFilter,
     );
+    gh.factory<_i847.ColorGeneratorService>(
+        () => _i847.ColorGeneratorServiceImpl());
     return this;
   }
 }
